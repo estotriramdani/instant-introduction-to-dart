c3d763315031dfa810563346a6a6c3dc4b1530ad
Esto Triramdani N <estotriramdani@gmail.com>
2021-01-25 16:08:11 +0700

    First Commmit

---
+++ 01-programpertama.dart
@@ -0,0 +1,4 @@
+void main() {
+  var variabelPertama = 'Ini variable pertama';
+  print('Hello World $variabelPertama');
+}
---
+++ 02-variable.dart
@@ -0,0 +1,12 @@
+void main() {
+  String nama = "Esto Triramdani N";
+  int umur = 21;
+  double tinggi = 1.7;
+  bool isMenikah = false;
+  var alamat = "Kp. Karikil, RT 01, RW 10, Desa Langonsari";
+
+  // mencetak variable
+  print("Nama saya $nama. Umur saya $umur. Tinggi saya $tinggi m");
+  print('Menikah: $isMenikah');
+  print("Alamat: $alamat");
+}
